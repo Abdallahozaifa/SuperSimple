1138a6ef844f094a5e199572ee305d0bac268ea8
Ken Schnall <kenneth.schnall@gmail.com>
2016-03-02 00:26:03 -0500

    add game.py, add player left right movement

---
+++ game.py
@@ -0,0 +1,42 @@
+import pygame
+from pygame.locals import *
+
+TITLE="SuperSimple"
+WIDTH=1366 # window width
+HEIGHT=768 # window height
+
+WHITE = (255,255,255) # variable for color white
+BLACK = (0,0,0) # variable for color black
+RED = (255,0,0) # variable for color red
+
+pygame.init() # initialize pygame
+
+gameDisplay = pygame.display.set_mode((WIDTH,HEIGHT)) # window resolution
+pygame.display.set_caption(TITLE) # window title
+
+gameExit = False # game has not been triggered to exit
+
+player_x = 300 # player x position
+PLAYER_Y = 300 # player y position, constant, does not change
+PLAYER_POTENTIAL_VELOCITY = 10 # potential player speed
+playerVelocity = 0 # current player speed
+
+while not gameExit: # game has not been triggered to exit
+	for event in pygame.event.get(): # for each user interaction with the game
+		if event.type == pygame.QUIT: # exit button clicked
+			gameExit = True # game is triggered to exit
+		if event.type == pygame.KEYDOWN: # a key is pressed
+			if event.key == pygame.K_LEFT: # left key is pressed
+				playerVelocity = -PLAYER_POTENTIAL_VELOCITY # move player left at player potential velocity
+			if event.key == pygame.K_RIGHT:
+				playerVelocity = PLAYER_POTENTIAL_VELOCITY
+
+	player_x += playerVelocity # move player left or right
+
+	gameDisplay.fill(WHITE) # background will be white
+	pygame.draw.rect(gameDisplay,BLACK,[player_x,PLAYER_Y,10,10]) # draw black rectangle at (400,300) (from top-left) with size (10,100) on top of background
+
+	pygame.display.update() # draw to window
+
+pygame.quit() # quit pygame
+quit() # quit python
